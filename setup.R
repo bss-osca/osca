534244cb72369406f3dbfffc4fa62fed0ba4f704
relund <junk@relund.dk>
2021-03-23 12:00:00 +0000

    test

--- setup.R
+++ setup.R
@@ -5,5 +5,5 @@ use_git_config(user.name = "relund", user.email = "junk@relund.dk")
 use_git()
 create_github_token()
 gitcreds::gitcreds_set()
-use_git_remote("origin", url = NULL, overwrite = TRUE)  # remove remote
-create_from_github
+git_remotes()
+use_git_remote("origin", url = "https://github.com/bss-osca/osca.git", overwrite = TRUE)  # set remote
