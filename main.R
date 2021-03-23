3bd8ad3c894f3f2818cd5361653c30b15aa9facf
relund <junk@relund.dk>
2021-03-23 12:38:06 +0000

    Commit from main

--- main.R
+++ main.R
@@ -1,5 +1,10 @@
-## Main file for generating 
+## Main file for generating html docs
+library(git2r)
+repo <- repository()
+
 #gs4_auth()
 rmarkdown::render("osca_diagramme.Rmd", output_file="diagramme.html", output_dir = "./docs/")
 rmarkdown::render("om.Rmd", output_file="om.html", output_dir = "./docs/")
 rmarkdown::render("osca.Rmd", output_file="osca.html", output_dir = "./docs/")
+
+commit(repo, "Commit from main")
