7c1eba85e463c19b0f0dab9c2afd05a864d33c30
Lars Relund Nielsen <junk@relund.dk>
2021-03-25 10:27:30 +0100

    Commit from main.R Thu Mar 25 10:27:30 2021

--- main.R
+++ main.R
@@ -2,6 +2,7 @@
 library(git2r)
 
 # gs4_auth()
+knitr::render()
 rmarkdown::render("osca_diagramme.Rmd", output_file="diagramme.html", output_dir = "./docs/")
 rmarkdown::render("om.Rmd", output_file="om.html", output_dir = "./docs/")
 rmarkdown::render("osca.Rmd", output_file="osca.html", output_dir = "./docs/")
