06a24a9e7e5a7785e0b13aeac673809c334bf5db
Lars Relund Nielsen <junk@relund.dk>
2021-03-26 11:59:32 +0100

    Remove generation

--- main.R
+++ main.R
@@ -1,12 +1,14 @@
 ## Main file for generating html docs
 library(git2r)
 
-# gs4_auth()
-rmarkdown::render("osca_diagramme.Rmd", output_file="diagramme.html", output_dir = "./docs/")
-rmarkdown::render("om.Rmd", output_file="om.html", output_dir = "./docs/")
-rmarkdown::render("osca.Rmd", output_file="osca.html", output_dir = "./docs/")
 
-repo <- repository()
-pull(repo)
-commit(repo, paste0("Commit from main.R ", date()), all = T)
-system("git push")
+# github actions do the job
+# # gs4_auth()
+# rmarkdown::render("osca_diagramme.Rmd", output_file="diagramme.html", output_dir = "./docs/")
+# rmarkdown::render("om.Rmd", output_file="om.html", output_dir = "./docs/")
+# rmarkdown::render("osca.Rmd", output_file="osca.html", output_dir = "./docs/")
+# 
+# repo <- repository()
+# pull(repo)
+# commit(repo, paste0("Commit from main.R ", date()), all = T)
+# system("git push")
